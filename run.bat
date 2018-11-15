89705a4af96f2bb302314a6ff718a92fb4319345
yfloe <yun-pang.floetteroed@dlr.de>
2018-11-15 14:20:08 +0100

    added extra flows

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-c:\python37\python.exe sumo_connector.py
+c:\python37\python.exe sumo_connector.py -s 129.247.218.121
--- run_test.bat
+++ run_test.bat
@@ -1,1 +1,1 @@
-c:\python37\python.exe test_sumo_connector.py
+c:\python37\python.exe test_sumo_connector.py 129.247.218.121 WorldForumTheHague
